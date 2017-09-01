701b8f13c1a2862f27ad18f6ae8fe399f60d0c0d
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-01 20:49:24 +0700

    CONG/4/5

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ const express = require('express');
 
 const app = express();
 
-// link route
-
 app.get('/hello/:name/:age', (req, res) => {
-    const name = req.params.name;
-    const age = req.params.age;
+    const { name, age } = req.params;
     res.send(`Xin chao toi la ${name}, ${age} tuoi`);
 });
 
+app.get('/tinh/:tenPhepTinh/:soA/:soB', (req, res) => {
+    const { tenPhepTinh, soA, soB } = req.params;
+    const pt = new Tinh(tenPhepTinh, soA, soB);
+    res.send(pt.getResultString());
+});
+
 app.listen(3000);
+
+class Tinh {
+    constructor(tenPhepTinh, soA, soB) {
+        this.tenPhepTinh = tenPhepTinh;
+        this.soA = soA;
+        this.soB = soB;
+    }
+
+    _getChuoiPhepTinh() {
+        const { tenPhepTinh, soA, soB } = this;
+        if (tenPhepTinh === 'CONG') return `${soA} + ${soB}`;
+        if (tenPhepTinh === 'TRU') return `${soA} - ${soB}`;
+        if (tenPhepTinh === 'NHAN') return `${soA} * ${soB}`;
+        return `${soA} / ${soB}`;
+    }
+
+    getResultString() {
+        const chuoiPhepTinh = this._getChuoiPhepTinh();
+        const result = eval(chuoiPhepTinh);
+        return `${chuoiPhepTinh} = ${result}`;
+    }
+}
